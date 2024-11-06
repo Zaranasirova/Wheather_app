8cf77dbea46ac91d04abb723ac92ad2c156266ef
Zaranasirova <zerifenesirova784@gmail.com>
2024-11-06 17:41:39 +0400

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import clear from "../Assets/image/clear.png";
 import cloud from "../Assets/image/cloud.png";
 import drizzle from "../Assets/image/drizzle.png";
@@ -6,11 +6,15 @@ import humidity from "../Assets/image/humidity.png";
 import rain from "../Assets/image/rain.png";
 import snow from "../Assets/image/snow.png";
 import wind from "../Assets/image/wind.png";
-import Search from './Search';
+import search_icon from "../Assets/image/search.png";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Hero = () => {
     const [wheatherData, setWheatherData] = useState(false);
-    const [searchValue, setSearchValue] = useState("Baku");
+    const [searchInput, setSearchInput] = useState("")
+
     const allIcons = {
         "01d": clear,
         "01n": clear,
@@ -29,7 +33,6 @@ const Hero = () => {
     }
 
     const search = async (city) => {
-       
 
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APP_ID}`;
@@ -45,38 +48,56 @@ const Hero = () => {
                 icon: icons
             })
         } catch (error) {
-            console.log(error)
+            setWheatherData(false)
+            console.error(" error in fetching weather data")
         }
     }
-    useEffect(() => {
-        search(searchValue);
-    }, [searchValue]);
+    const handleSearch = () => {
+        if (searchInput === "") {
+            toast.error("City name cannot be empty!");
+            return;
+        }
+        search(searchInput)
+    }
+
+
 
 
     return (
         <div className="hero">
-            <Search setSearchValue={setSearchValue} />
-            <div className='weather-top-content'>
-                <img src={wheatherData.icon} alt="clear" />
-                <p className='temprature'>{wheatherData.temprature}°C</p>
-                <p className='location'>{wheatherData.location}</p>
+            <div className='search-wrapper'>
+                <input type="text" placeholder='City name' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                <img src={search_icon} alt="search_icon" onClick={handleSearch} />
             </div>
-            <div className="weather-data">
-                <div className="col">
-                    <img src={humidity} alt="humidity" />
-                    <div>
-                        <p>{wheatherData.humidity}%</p>
-                        <span>Humidity</span>
-                    </div>
-                </div>
-                <div className="col">
-                    <img src={wind} alt="wind" />
-                    <div>
-                        <p>{wheatherData.windSpeed} Km/h</p>
-                        <span>Wind Speed</span>
-                    </div>
-                </div>
-            </div>
+            {
+                wheatherData && (
+                    <>
+                        <div className='weather-top-content'>
+                            <img src={wheatherData.icon} alt="clear" />
+                            <p className='temprature'>{wheatherData.temprature}°C</p>
+                            <p className='location'>{wheatherData.location}</p>
+                        </div>
+                        <div className="weather-data">
+                            <div className="col">
+                                <img src={humidity} alt="humidity" />
+                                <div>
+                                    <p>{wheatherData.humidity}%</p>
+                                    <span>Humidity</span>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <img src={wind} alt="wind" />
+                                <div>
+                                    <p>{wheatherData.windSpeed} Km/h</p>
+                                    <span>Wind Speed</span>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+
+            <ToastContainer />
         </div>
     )
 }
